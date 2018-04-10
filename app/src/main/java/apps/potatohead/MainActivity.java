821108c175d01bb020b64da8e637ac0318596836
Dante <d.niewenhuis@hotmail.com>
2018-04-10 14:34:23 +0200

    made potato app for portrait mode and started with landscape mode

--- app/src/main/java/apps/potatohead/MainActivity.java
+++ app/src/main/java/apps/potatohead/MainActivity.java
@@ -2,6 +2,10 @@ package apps.potatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean DEBUG = false;
+
+    public void checkClicked(View v) {
+        CheckBox checkbox = (CheckBox) v;
+        String img_id = checkbox.getText().toString() + "_img";
+        if (DEBUG) {
+            Log.d("id_checker","The image id = " + img_id);
+        }
+
+        int resId = getResources().getIdentifier(img_id, "id", getPackageName());
+        ImageView image = findViewById(resId);
+        if (checkbox.isChecked()) {
+            if (DEBUG){
+                Log.d("clicked_checker",checkbox.getText().toString() + " is checked");
+            }
+
+            // make image visible
+            image.setVisibility(View.VISIBLE);
+        }
+        else {
+            if (DEBUG){
+                Log.d("clicked_checker",checkbox.getText().toString() + " is not checked");
+            }
+
+            // make image invisible
+            image.setVisibility(View.INVISIBLE);
+        }
     }
 }
