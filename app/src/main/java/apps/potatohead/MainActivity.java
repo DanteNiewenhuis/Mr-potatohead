f5394541612dbf6a0e523767509df6ea92d05b90
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2018-04-11 23:16:33 +0200

    saving is now functional. Still needs to add the option to safe a style as an extra.

--- app/src/main/java/apps/potatohead/MainActivity.java
+++ app/src/main/java/apps/potatohead/MainActivity.java
@@ -6,29 +6,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean DEBUG = false;
+    private String[] img_list = {"body_img", "arms_img", "ears_img", "eyes_img", "eyebrows_img",
+                                 "shoes_img", "nose_img", "mouth_img", "hat_img", "mustache_img"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("create_checker", "check");
-
-
-        if (not_init) {
-            ImageView body = findViewById(R.id.body_img);
-            Log.d("not_init checker", "check");
-            body.setVisibility(savedInstanceState.getInt("body_img"));
-        }
-        else {
-            Log.d("init_checker", "check");
-            not_init = true;
+        if (savedInstanceState != null) {
+            for (String img_id: img_list) {
+                int resId = getResources().getIdentifier(img_id, "id", getPackageName());
+                ImageView image = findViewById(resId);
+                image.setVisibility(savedInstanceState.getInt(img_id));
+            }
         }
     }
+    public void safeLayout(View v) {
 
-    private boolean DEBUG = false;
-    private boolean not_init = false;
+    }
+
+
 
     public void checkClicked(View v) {
         CheckBox checkbox = (CheckBox) v;
@@ -59,34 +61,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState); // always call super
-        Log.d("save_checker", "check");
-        outState.putInt("body_img", View.VISIBLE);
-
-//        HashMap<String, Boolean> visibility_map = new HashMap<String, Boolean>();
-//        static(
-//                visibility_map.put("body_img", false);
-//            visibility_map.put("arms_img", false);
-//            visibility_map.put("ears_img", false);
-//            visibility_map.put("mustache_img", false);
-//            visibility_map.put("mouth_img", false);
-//            visibility_map.put("eyes_img", false);
-//            visibility_map.put("hat_img", false);
-//            visibility_map.put("nose_img", false);
-//            visibility_map.put("shoes_img", false);
-//            visibility_map.put("eyebrows_img", false);
-//        )
-//
-//        for (String img_id : visibility_map.keySet()) {
-//            Log.d("name_cheker", img_id);
-//            int resId = getResources().getIdentifier(img_id, "id", getPackageName());
-//            ImageView image = findViewById(resId);
-//            if (image.getVisibility() == View.VISIBLE) {
-//                Log.d("visibility_cheker", "is visible");
-//                visibility_map.put(img_id, true);
-//            } else {
-//                Log.d("visibility_cheker", "is not visible");
-//                visibility_map.put(img_id, false);
-//            }
-//        }
+        HashMap<String, boolean> last_save = new HashMap<String, boolean>();
+        outState.putSerializable("last_save", last_save);
+        for (String img_id: img_list) {
+            int resId = getResources().getIdentifier(img_id, "id", getPackageName());
+            ImageView image = findViewById(resId);
+            outState.putInt(img_id, image.getVisibility());
+        }
     }
 }
